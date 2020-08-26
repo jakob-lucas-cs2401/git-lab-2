65261d929f602154de2d691f0c3bae6b5a0e67fb
jakob-lucas-cs2401 <jl930219@ohio.edu>
2020-08-26 14:54:48 -0400

    Initial Commit

---
+++ git-pab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jakob Lucas
+ *        Date: 08-26-20
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
